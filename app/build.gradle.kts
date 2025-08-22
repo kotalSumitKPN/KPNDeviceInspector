0175e6b32390683b2a190fa7fd7e54eeb0463a98
Sumit Kotal <sumit.kotal@kpnfarmfresh.com>
2025-08-22 15:57:08 +0530

    settings changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,9 +23,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
