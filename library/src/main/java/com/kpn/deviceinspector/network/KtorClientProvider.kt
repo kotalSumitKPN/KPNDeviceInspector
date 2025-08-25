28e7611e6a637496dc48dd3e15c658c2b403ef26
Sumit Kotal <sumit.kotal@kpnfarmfresh.com>
2025-08-25 11:10:21 +0530

    Logger changes

--- library/src/main/java/com/kpn/deviceinspector/network/KtorClientProvider.kt
+++ library/src/main/java/com/kpn/deviceinspector/network/KtorClientProvider.kt
@@ -3,6 +3,7 @@ package com.kpn.deviceinspector.network
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -21,7 +22,7 @@ object KtorClientProvider {
         }
 
         install(Logging) {
-            logger = Logger.SIMPLE
+            logger = Logger.ANDROID
             level = LogLevel.ALL
         }
     }
